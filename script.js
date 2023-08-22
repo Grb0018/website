b5e83075532b85989a14df292e104d11a6dd9c89
Grb0018 <gourab.bitblits@gmail.com>
2023-08-22 19:28:29 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,67 @@
+window.onload=()=>{
+
+    for (let i = 0; i < 120; i++) {
+        var span = document.createElement('span');
+        span.setAttribute('class','star'); 
+        var top = Math.floor(Math.random()*100)+'vh';
+        var left = Math.floor(Math.random()*100)+'vw';
+        var types=['on','off'];
+        if(types[Math.floor(Math.random()*2)]=='on'){
+            span.style.opacity = '0.3'
+            span.style.animation = 'star 2s linear infinite';
+        }
+        span.style.left = left;
+        span.style.top = top;
+        document.body.querySelector('main').append(span);
+    }
+
+    var img = document.createElement('img');
+    img.src='./asset/meteor.png';
+    img.style.position = 'absolute';
+    img.style.transition = 'all 8s linear';
+    img.style.width = '10vw';
+    img.style.transform = 'translate(-60vw, 10vw)';
+    document.body.querySelector('main').append(img);
+    setTimeout(()=>{
+        img.style.transform = 'translate(64vw, -25vw)';
+    },800)
+
+    var img1 = document.createElement('img');
+    img1.src='./asset/meteor.png';
+    img1.style.position = 'absolute';
+    img1.style.transition = 'all 4s linear';
+    img1.style.width = '10vw';
+    img1.style.transform = 'translate(-60vw, 20vw)';
+    document.body.querySelector('main').append(img1);
+    setTimeout(()=>{
+        img1.style.transform = 'translate(64vw, -20vw)';
+    },1800)
+
+    setTimeout(()=>{
+        document.getElementById('rocket1').style.transform = 'rotate(360deg) translate(15vw,5vw)';
+        setTimeout(()=>{
+            document.getElementById('rocket1').style.transition = 'unset';
+            document.getElementById('rocket1').style.animation = 'rocket1 20.4s linear infinite';
+        },7495)
+    },800)
+
+    setTimeout(()=>{
+        document.getElementById('rocket2').style.transform = 'rotate(0deg) translate(-18vw, 2vw)';
+        setTimeout(()=>{
+            document.getElementById('rocket2').style.transition = 'unset';
+            document.getElementById('rocket2').style.animation = 'rocket2 25.4s linear infinite';
+        },7495)
+    },800)
+
+}
+setTimeout(()=>{
+    document.getElementById('typed').style.visibility = 'visible';
+    var typed = new Typed(".text-slider", {
+        strings: ["hi","Welcome to Bitblits Digital Workstation","Do you want to access the website ?",`<button onclick="window.open('http://bitblits.in/DIGITAL-BROCHUR/')" style='width: 5vw;background: #ffc47d;border: 0;filter: drop-shadow(2px 1px 1px grey);font-size: 1vw;'>Yes</button>&nbsp;&nbsp;&nbsp;&nbsp;<button style='width: 5vw;background: #f39999;border: 0;filter: drop-shadow(2px 1px 1px grey);font-size: 1vw;'>Later</button>`],
+        typeSpeed: 100,
+        backSpeed: 50,
+        loop: false,
+        cursorChar: ''
+    });
+},6000)
+
